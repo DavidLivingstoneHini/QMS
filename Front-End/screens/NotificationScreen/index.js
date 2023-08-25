1c0350c989269f15423216d4b4022a382516de19
Livingstone David Kwame Hini <kwamelivingstone77@gmail.com>
2023-08-25 01:43:19 +0000

    Update index.js

--- Front-End/screens/NotificationScreen/index.js
+++ Front-End/screens/NotificationScreen/index.js
@@ -1,7 +1,12 @@
 import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
+import { Text, StyleSheet, Image, View, TouchableOpacity, Button, Platform } from 'react-native';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {BottomNav} from "../../components/BottomNav"
+import { Txt } from '../../components/Txt';
+import sun from "../../assets/images/sun.png"
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -41,18 +46,27 @@ export const NotificationScreen = ({navigation}) => {
         alignItems: 'center',
         justifyContent: 'space-around',
       }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        <View  style={styles.header1}>
+        <View style={{flexDirection: "row", alignItems: "center", marginTop: 4}}>
+        <Image source={sun} style={styles.img}/>
+          <Txt style={styles.greeting}>QUMI</Txt>
+        </View>
+          <Text style={{fontSize: 25, fontWeight: "bold", textAlign: "center", marginTop: 5, color: "white"}}>Norifications</Text>
+        </View>
+      {/* <Text style={{fontSize: 17, fontWeight: "bold", marginTop: -30}}>Recent Notifications</Text> */}
+      <View style={{ alignItems: 'center', justifyContent: 'center', borderStyle: 'solid', borderWidth: 1, borderColor: "black", borderRadius: 18, backgroundColor: "white", paddingHorizontal: 20, paddingVertical: 12 }}>
+        <Text>{notification && notification.request.content.title}</Text>
+        <Text>{notification && notification.request.content.body}</Text>
+        <Text>{notification && JSON.stringify(notification.request.content.data)}</Text>
       </View>
       <Button
-        title="Press to schedule a notification"
+        title="Tap to refresh"
         onPress={async () => {
           await schedulePushNotification();
         }}
       />
+              <BottomNav navigation={navigation}/>
+
     </View>
   );
 }
@@ -60,8 +74,8 @@ export const NotificationScreen = ({navigation}) => {
 async function schedulePushNotification() {
   await Notifications.scheduleNotificationAsync({
     content: {
-      title: "You've got mail! 📬",
-      body: 'Here is the notification body',
+      title: "Joined queue successfully! 📬",
+      body: 'Please make sure to check your queue position',
       data: { data: 'goes here' },
     },
     trigger: { seconds: 2 },
@@ -99,3 +113,36 @@ async function registerForPushNotificationsAsync() {
 
   return token;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 40,
+  },
+  header1: {
+    backgroundColor: "#167bf0",
+height: 100,
+alignItems: "center",
+justifyContent: "center",
+flexDirection: "column",
+paddingHorizontal: 40,
+width: "100%",
+borderBottomLeftRadius: 40,
+borderBottomRightRadius: 40,
+top: -98
+  },
+  greeting: {
+    fontSize: 24,
+    color: "#fff"
+  },
+  img: {
+    width: 40,
+    height: 40,
+  },
+  companies: {
+    width: "100%",
+    marginVertical: 60,
+    
+  }
+});
