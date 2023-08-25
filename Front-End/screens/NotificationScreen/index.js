5859fe802db4540eb72ebb7b770820e9a782bae1
Livingstone David Kwame Hini <kwamelivingstone77@gmail.com>
2023-08-25 03:20:24 +0000

    Update index.js

--- Front-End/screens/NotificationScreen/index.js
+++ Front-End/screens/NotificationScreen/index.js
@@ -11,7 +11,7 @@ import sun from "../../assets/images/sun.png"
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
+    shouldPlaySound: true,
     shouldSetBadge: false,
   }),
 });
